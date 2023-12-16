71012f16a1aeec28850988c8934246440c6f3211
Masroor Ehsan <masroore@gmail.com>
2023-12-16 10:10:01 +0600

    merge 516

---
+++ merge_test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Mergecom;
+
+namespace merge_test
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var iniPath = "./config/merge.ini";
+            var localAE = "SUPR_PACS";
+
+            var finfo = new FileInfo(iniPath);
+            MC.mcInitialization(finfo);
+            var app = MCapplication.getApplication(localAE);
+        }
+    }
+}
